68b292b286e280aa99668a964cd89dbabb7f568c
Михаил <dda3127@gmail.com>
2023-11-16 18:04:43 +0200

    Grid and list of songs + search page

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,21 @@
+import { clear } from "console";
+import exp from "constants";
+import { useEffect, useState } from "react";
+
+function useDebounce<T>(value: T, delay?: number): T {
+  const [debounceValue, setDebounceValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay || 500);
+
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [value, delay]);
+
+return debounceValue;
+}
+
+export default useDebounce;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "gxlnilzexoikozaojanb.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
